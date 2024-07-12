f89b46a5b5d7402cfa3ec857be8d6e65aa8a2792
NimajnebEC <46959407+nimajnebec@users.noreply.github.com>
2024-07-12 16:39:02 +0100

    feat: autoclose

--- src-rs/src/main.rs
+++ src-rs/src/main.rs
@@ -1,8 +1,21 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::utils::config::WindowConfig;
+
+const DEBUG: bool = cfg!(debug_assertions);
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .setup(|app| {
+            tauri::WindowBuilder::from_config(app, WindowConfig::default())
+                .title("Fordingbridge Astronomers Gallery")
+                .always_on_top(!DEBUG)
+                .fullscreen(!DEBUG)
+                .focused(true)
+                .build()?;
+            return Ok(());
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,12 @@
 <script>
 	import Slideshow from "./lib/Slideshow.svelte";
+	import { exit } from "@tauri-apps/api/process";
+
+	async function close() {
+		if (import.meta.env.MODE != "development") await exit(1);
+	}
 </script>
+
+<svelte:window on:pointermove={close} on:pointerdown={close} on:keydown={close} />
 
 <Slideshow />
